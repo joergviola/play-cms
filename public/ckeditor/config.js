424f9c3ab847840f4c450f54e4095f7716bf6bb6
Vadim Gerassimov <vadim@codeborne.com>
2012-10-19 11:18:18 +0300

    replacing tiny_mce with more advanced ckeditor

---
+++ public/ckeditor/config.js
@@ -0,0 +1,18 @@
+﻿CKEDITOR.editorConfig = function(config) {
+  config.removePlugins += 'about,allyhelp,button,print,maximize,newpage,pagebreak,resize,scayt,smiley,wsc,' + config.removePlugins;
+  config.toolbar_Full =
+      [
+        ['Source','-','Preview','-','ShowBlocks'],
+        ['Link','Unlink','Anchor'],
+        ['SpecialTag','Image','Table','SpecialChar'],
+        ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
+        '/',
+        ['FontSize','TextColor','BGColor'],
+        ['Bold','Italic','Underline','Strike'],
+        ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+        ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+        ['RemoveFormat']
+      ];
+
+  config.language = 'ru';
+};
