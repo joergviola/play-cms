126223904cbf6fcad129ac114618da67de0d8e98
Anton Keks <anton@codeborne.com>
2012-11-16 18:30:46 +0200

    add 30mn (by default) cache for cms pages

--- app/models/cms/CMSPage.java
+++ app/models/cms/CMSPage.java
@@ -1,7 +1,10 @@
 package models.cms;
 
 import org.hibernate.annotations.Type;
+import play.Play;
+import play.cache.Cache;
 import play.data.validation.Required;
+import play.db.jpa.JPABase;
 import play.db.jpa.Model;
 
 import javax.persistence.Column;
@@ -15,6 +18,9 @@ import static javax.persistence.TemporalType.TIMESTAMP;
 
 @Entity
 public class CMSPage extends Model {
+  public static final CMSPage NULL_PAGE = new CMSPage();
+  public static final String CACHE_EXPIRATION = Play.configuration.getProperty("cms.cache.expiration", "30mn");
+
 	@Required public String name;
   public String locale;
   @Required public String title;
@@ -30,11 +36,42 @@ public class CMSPage extends Model {
 	public Integer sort;
 	public String tags;
 
-	public static List<CMSPage> findByTag(String tag) {
+  boolean isNullPage() {
+    return id == null && name == null && body == null;
+  }
+
+  static String cacheKey(String name, String locale) {
+    return "cms:" + name + ":" + locale;
+  }
+
+  String cacheKey() {
+    return cacheKey(name, locale);
+  }
+
+  @Override public <T extends JPABase> T save() {
+    T page = super.save();
+    Cache.set(cacheKey(), page, CACHE_EXPIRATION);
+    return page;
+  }
+
+  @Override public <T extends JPABase> T delete() {
+    T page = super.delete();
+    Cache.delete(cacheKey());
+    return page;
+  }
+
+  public static List<CMSPage> findByTag(String tag) {
 		return find("tags like ? order by sort", "%" + tag + "%").fetch();
 	}
 
   public static CMSPage findByName(String name, String locale) {
-    return find("byNameAndLocale", name, locale).first();
+    String key = cacheKey(name, locale);
+    CMSPage page = (CMSPage) Cache.get(key);
+    if (page == null) {
+      page = find("byNameAndLocale", name, locale).first();
+      if (page == null) page = NULL_PAGE;
+      Cache.set(key, page, CACHE_EXPIRATION);
+    }
+    return page.isNullPage() ? null : page;
   }
 }
