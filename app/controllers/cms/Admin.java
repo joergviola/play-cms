7069e7f2b68cb750539efc0efb6e16fc2eecb701
Maksim Säkki <maksim@codeborne.com>
2013-02-01 11:52:01 +0200

    added possibility to hook after save/delete events

--- app/controllers/cms/Admin.java
+++ app/controllers/cms/Admin.java
@@ -7,10 +7,12 @@ import play.data.validation.Valid;
 import play.i18n.Lang;
 import play.mvc.Controller;
 import play.mvc.Router;
+import play.utils.Java;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Date;
 import java.util.List;
 
@@ -65,6 +67,7 @@ public class Admin extends Controller {
 
     if (request.params.get("delete") != null) {
       page.delete();
+      Extension.invoke("afterDelete", page);
       index();
     }
 
@@ -72,6 +75,7 @@ public class Admin extends Controller {
       renderTemplate("@edit", page);
 
 		page.save();
+    Extension.invoke("afterSave", page);
 		if (request.params.get("savePage") != null)
 			Frontend.show(null, page.name);
 		index();
@@ -109,4 +113,19 @@ public class Admin extends Controller {
 		List<CMSImage> images = CMSImage.findAll();
 		render(images);
 	}
+
+  public static class Extension extends Controller {
+    static void afterSave(CMSPage page) {}
+
+    static void afterDelete(CMSPage page) {}
+
+    private static Object invoke(String m, Object... args) throws Throwable {
+      try {
+        return Java.invokeChildOrStatic(Extension.class, m, args);
+      }
+      catch (InvocationTargetException e) {
+        throw e.getTargetException();
+      }
+    }
+  }
 }
