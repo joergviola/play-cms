55998d5cdf33ff2c5deba6b348c39157d87bb0c0
Andrei Solntsev <andrei.solntsev@gmail.com>
2016-05-04 22:41:47 +0300

    play-cms 1.5: Use play built-in method Java.invokeChildOrStatic
    ... instead of re-inventing the wheel

--- app/controllers/cms/Profiler.java
+++ app/controllers/cms/Profiler.java
@@ -1,10 +1,6 @@
 package controllers.cms;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.List;
-
 import controllers.Secure.Security;
-
 import play.Logger;
 import play.Play;
 import play.mvc.Scope.Session;
@@ -37,21 +33,8 @@ public class Profiler {
 			return false;
 		}
 	}
-	
-	private static Object invoke(Class<?> original, String m, Object... args) throws Throwable {
-		Class called = null;
-		List<Class> classes = Play.classloader
-				.getAssignableClasses(original);
-		if (classes.size() == 0) {
-			called = original;
-		} else {
-			called = classes.get(0);
-		}
-		try {
-			return Java.invokeStaticOrParent(called, m, args);
-		} catch (InvocationTargetException e) {
-			throw e.getTargetException();
-		}
-	}
-	
+
+  private static Object invoke(Class<?> original, String m, Object... args) throws Throwable {
+    return Java.invokeChildOrStatic(original, m, args);
+  }
 }
